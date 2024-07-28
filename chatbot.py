eaed532322a0f8d7029570ac04e2a901655e187f
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-07-27 21:05:34 -0300

    treinando o chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,18 @@
 from chatterbot import ChatBot 
 from chatterbot.trainers import ListTrainer
+
+chatbot = ChatBot("SnakeBot")
+
+conversation = [
+    "Opa",
+    "E aí, tranquilo?",
+    "Tranquilo",
+    "Terminou AWS?",
+    "Eu não, e vc?",
+    "Claro que não",
+    "Decepcionante",
+    "Eu sei"
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
